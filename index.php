bd34d80dcbd661129883e54b948c4003d8079a82
Gerben Reppel <gerbenreppel@hotmail.com>
2021-03-02 14:27:32 +0100

    The beginning of the project. This mostly contains comments in the php file to guide me.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List</title>
+</head>
+<body>
+	<header>ToDo list</header>
+
+	<div>
+		<div>List name</div><!-- When clicked the user can write in this field and change the name -->
+		<div>task 1</div><!-- removed when user leaves it blank -->
+
+		<button>add button</button><!-- adds a task -->
+		<button>remove button</button><!-- removes the list -->
+	</div>
+
+	
+	<button>add button</button><!-- adds a list -->
+
+</body>
+</html>
