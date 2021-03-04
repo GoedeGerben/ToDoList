e8cba5bd1ed40f7c2baf2ec3600a272684b1b73e
Gerben Reppel <gerbenreppel@hotmail.com>
2021-03-04 20:59:27 +0100

    input fields + textarea style

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>ToDo List</title>
 </head>
 <body>
+	<?php
+
+	$list1 = "List one";
+	$task1 = "task one";
+	?>
 	<header>ToDo list</header>
 
-	<div>
-		<div>List name</div><!-- When clicked the user can write in this field and change the name -->
-		<div>task 1</div><!-- removed when user leaves it blank -->
+	<div class="list">
+		<form>
+			<input type="text" id="list1" name="list1" maxlength="25" value="<?php echo $list1 ?>">
+			<input type="text" id="task1" name="task1" maxlength="25" value="<?php echo $task1 ?>">
+		</form>
 
 		<button>add button</button><!-- adds a task -->
 		<button>remove button</button><!-- removes the list -->
