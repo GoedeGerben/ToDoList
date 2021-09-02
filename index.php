254e56c94a6e2a1c7dbe53d670d00e96cacc904e
Gerben <99052865@mydavinci.nl>
2021-09-02 10:35:52 +0200

    Tasks for lists on page
    The tasks for a list are posted under the list. bugged.

--- index.php
+++ index.php
@@ -7,33 +7,26 @@ $dbname = 'todolist';
 $dsn = 'mysql:host='. $host .';dbname='. $dbname;
 
 $pdo = new PDO($dsn, $user, $password);
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);//zorgt ervoor dat je niks in de fetch() moet zetten
 
+
+//begin van alles op de pagina zetten
+//selecteert alles uit 'lijsten' en zet het op de pagina
 $stmt = $pdo->query('SELECT * FROM lijsten');
 
 while ($row = $stmt->fetch()) {
-echo $row->naam . '<br>';
+	$lijstid = $row->id;
+	echo $row->naam . '<br>';
+
+	//selecteert elke taak die bij de lijst hoort die momenteel door de loop gaat
+	$sql = 'SELECT * FROM taken WHERE lijstid = ?';
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute([$lijstid]);
+	$taken = $stmt->fetchALL();
+
+	foreach ($taken as $taken) {
+		echo $taken->naam . '<br>';
+	}
+	echo '<br>';
 }
-
-//pakt alleen degene met de kleur geel op een veilige manier
-$kleur = 'geel';
-
-$sql = 'SELECT * FROM lijsten WHERE kleur = ?';
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$kleur]);
-$lijsten = $stmt->fetchALL();
-
-foreach ($lijsten as $lijsten) {
-	echo $lijsten->naam . '<br>';
-}
-//einde van de data oproep en zetten op de pagina
-
-
-
-
-
-
-
-
-
 ?>
